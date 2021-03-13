82fc1ac3b3b6aac60a4881dc243d77dbe30b2043
Saiyam <saiyam@vinsol.com>
2021-03-14 02:58:20 +0530

    Small fix

--- src/components/player.js
+++ src/components/player.js
@@ -5,14 +5,10 @@ const player = (props) => {
   let formatValue = (value) => value.toFixed(0);
   return (
     <div className={`Player ${props.type}`}>
-      {props.maxHealth > 0 ?
-        <>
-        <h1 className="PlayerType">{props.type} <br />
-          <AnimatedNumber value={props.maxHealth} duration="500" formatValue={formatValue} />
-        </h1>
-        <span className="Background" style={{width: props.maxHealth + '%'}}></span>
-        </>
-        : null}
+      <h1 className="PlayerType">{props.type} <br />
+        <AnimatedNumber value={props.maxHealth} duration="500" formatValue={formatValue} />
+      </h1>
+      <span className="Background" style={{width: props.maxHealth + '%'}}></span>
     </div>
   );
 };
